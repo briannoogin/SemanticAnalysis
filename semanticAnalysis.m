c1fd001d92823b5342c96e2fc21d03eb9b95afb5
Brian Nguyen <briannoogin@yahoo.com>
2016-08-14 14:53:38 -0500

    Fixed Descrption and Added Matlab File

---
+++ semanticAnalysis.m
@@ -0,0 +1,6 @@
+% This program takes string input and performs matrix operations to find
+% similarities between words. 
+% Written by Brian Nguyen
+
+% Makes a cell array based on text input
+semanticMatrix = 
