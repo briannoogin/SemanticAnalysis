3561d89addbb37a60eb0d71732e9749d06bedbed
Brian Nguyen <briannoogin@yahoo.com>
2016-08-15 14:32:08 -0500

    Added second column to semantic matrix
    It still is a work in progress. I am trying to merge repeated words from
    both passages.

--- semanticAnalysis.m
+++ semanticAnalysis.m
@@ -12,27 +12,34 @@ textVector = textMatrix{1,1};
 fclose(fileID);
 semanticMatrix = cell(size(textVector,1),2);
 indexVector = cell(size(textVector,1),1);
+noteIndexVector = cell(size(noteVector,1),1);
 indexOfTrue = 1;
+
+% Instagram Update Description
+noteID = fopen('instagramUpdateNote.txt');
+noteMatrix = textscan(noteID,'%s');
+noteVector = noteMatrix{1,1};
+fclose(noteID);
 
 % Loops through the textVector and finds unique words in the passage and
 % puts it in semanticMatrix along with the number of occurences in the
 % passage.
 for index = 1 : size(textVector,1)
-    word = textVector{index};
+    textWord = textVector{index};
     % Checks if there is a blank in the matrix because some entries were
     % cleared
-    if(~strcmp(word,''))
+    if(~strcmp(textWord,''))
         % This for loop replaces strfind since strfind finds strings within
         % other strings. I do not want to find strings within other
         % strings, so I am comparing the whole string with another whole
         % string.
         for vectorIndex = 1:size(indexVector,1)
-            if(strcmp(word,textVector{vectorIndex,1}))
+            if(strcmp(textWord,textVector{vectorIndex,1}))
                 indexVector{vectorIndex,1} = 1;
             end
         end
         nonZeroIndexVector = cell2mat(indexVector);
-        semanticMatrix{index,1} = word;
+        semanticMatrix{index,1} = textWord;
         semanticMatrix{index,2} = size(nonZeroIndexVector,1);
         trueIndex = cell(size(nonZeroIndexVector,1),1);
         indexOfTrue = 1;
@@ -53,10 +60,27 @@ for index = 1 : size(textVector,1)
         indexVector = cell(size(textVector,1),1);
     end
 end
-
+noteWordIndex =  size(indexVector,1);
+for noteIndex = 1 : size(noteVector,1)
+    noteWord = noteVector{noteIndex,1};
+     if(~strcmp(noteWord,''))
+        for noteVectorIndex = 1:size(noteIndexVector,1) 
+            if(strcmp(noteWord,noteVector{noteVectorIndex,1}))
+                noteIndexVector{noteVectorIndex,1} = 1;
+            end
+        end
+     nonZeroIndexVector = cell2mat(noteIndexVector);
+     semanticMatrix{noteWordIndex,3} = size(nonZeroIndexVector,1);
+     semanticMatrix{noteWordIndex,1} = noteWord;
+     noteWordIndex = noteWordIndex + 1;
+     end
+     % Clears noteVector
+     noteIndexVector = cell(size(noteVector,1),1);
+end
 % Removes entries with blanks
 semanticMatrix(all(cellfun(@isempty,semanticMatrix),2),:) = [];
 
+%{
 % Data Transformation through logs
 numericSemantic = cell2mat(semanticMatrix(:,2));
 semanticMatrix(:,2) = num2cell(log2(numericSemantic));
@@ -64,4 +88,4 @@ entropy = -1 .* numericSemantic .* log2(numericSemantic);
 %semanticMatrix(:,2) = num2cell(cell2mat(semanticMatrix(:,2)) ./ entropy);
 [i,j,k] = svd(cell2mat(semanticMatrix(:,2)));
 
-
+%}
