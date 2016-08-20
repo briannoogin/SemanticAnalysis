ae3f4ea6ef31c0ba6c757dce86f92bc8ae964273
Brian Nguyen <briannoogin@yahoo.com>
2016-08-19 22:35:18 -0500

    Added inputText function
    By writing a inputText function, it is easier to add new documents. The
    occurence method of semanticAnalysis will be written in a new function
    later.

---
+++ inputText.m
@@ -0,0 +1,59 @@
+% This function handles the input of text.
+% By Brian Nguyen
+function [outputMatrix] = inputText(textName,inputMatrix)
+% Scans the text file
+fileID = fopen(textName);
+scanMatrix = textscan(fileID,'%s');
+textVector = scanMatrix{1,1};
+fclose(fileID);
+
+% Adds the new passage to the matrix
+
+textMatrix = cat(1,inputMatrix,textVector);
+% Takes the original size of the inputMatrix and adds an extra column for
+% the new dataset
+outputMatrix = cell(size(textMatrix,1),size(textMatrix,2) + 1);
+indexVector = cell(size(textMatrix,1),1);
+
+% Loops through the wordVector and finds unique words in the passage and
+% puts it in semanticMatrix.
+for index = 1 : size(textMatrix,1)
+    textWord = textMatrix{index,1};
+    % Checks if there is a blank in the matrix because some entries were
+    % cleared
+    if(~strcmp(textWord,''))
+        % This for loop replaces strfind since strfind finds strings within
+        % other strings. I do not want to find strings within other
+        % strings, so I am comparing the whole string with another whole
+        % string.
+        for vectorIndex = 1:size(indexVector,1)
+            if(strcmp(textWord,textMatrix{vectorIndex,1}))
+                indexVector{vectorIndex,1} = 1;
+            end
+        end
+        nonZeroIndexVector = cell2mat(indexVector);
+        outputMatrix{index,1} = textWord;
+        trueIndex = cell(size(nonZeroIndexVector,1),1);
+        indexOfTrue = 1;
+        % Loops through the indexVector and finds nonzeros which corresponds to
+        % the index of an occurence of a word.
+        for indexOfNonEmpty = 1:size(indexVector)
+            if(~isempty(indexVector{indexOfNonEmpty}))
+                trueIndex{indexOfTrue} = indexOfNonEmpty;
+                indexOfTrue = indexOfTrue + 1;
+            end  
+        end
+        % Loops through text vector and clears all the next entries that
+        % have the same word.
+        for deleteIndex = 1: size(trueIndex)
+            textMatrix{trueIndex{deleteIndex,1},1} = '';
+        end
+        % Clears indexVector
+        indexVector = cell(size(textMatrix,1),1);
+    end
+end
+% Clears all blank entries
+outputMatrix(all(cellfun(@isempty,outputMatrix),2),:) = [];
+
+
+    
---
+++ inputTextTest.m
@@ -0,0 +1,7 @@
+inputMatrix = cell(0,0);
+%inputMatrix{1,1} = 'swag';
+%inputMatrix{1,2} = 4;
+textName = 'instagramDescription.txt';
+testMatrix = inputText(textName,inputMatrix);
+
+
